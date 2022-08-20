e01006f980cc5fb2ff320f7f03d775af47aae5e5
Минтюков Захар <ao34ft@gmail.com>
2022-08-20 22:00:37 +1000

    «Добавлены urls и view-функции»

--- anfisa/anfisa/urls.py
+++ anfisa/anfisa/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 # импорт include позволит использовать адреса, включенные в приложения
-from django.urls import include, path 
+from django.urls import include, path
 
 urlpatterns = [    
     # Сначала проверяем все пути, которые есть в приложении ice_cream
